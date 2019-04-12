105cf74b7e37bfb898338be0f17bc1108c452513
only2433 <only2433@naver.com>
2019-04-12 15:45:56 +0900

    Initial commit

---
+++ tireTownSchool1/src/main/java/com/littlefox/storybook/tiretownschool1/MainApplication.java
@@ -0,0 +1,15 @@
+package com.littlefox.storybook.tiretownschool1;
+
+import android.app.Application;
+import android.util.DisplayMetrics;
+
+public class MainApplication extends Application{
+	
+	@Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+	
+	
+}
